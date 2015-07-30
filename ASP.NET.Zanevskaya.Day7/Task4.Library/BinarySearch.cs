19e6b36bc7220f8019b3be082a1cb503ce67b46d
Ann Zanevskaya <ann.zanevskaya7@gmail.com>
2015-07-30 10:29:20 +0300

    TBinarySearch

---
+++ ASP.NET.Zanevskaya.Day7/Task4.Library/BinarySearch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.Library
+{
+    public class BinarySearch
+    {
+        public static int Binary<T>(T[] array, T key)
+        {
+            return BinaryFinder(array, key, 0, array.Length, null);
+        }
+        public static int Binary<T>(T[] array, T key, IComparer<T> comparer)
+        {
+            if (ReferenceEquals(comparer, null)) throw new ArgumentNullException("comparer");
+            return Binary<T>(array, key, comparer.Compare);
+        }
+        public static int Binary<T>(T[] array, T key, Comparison<T> comparison)
+        {
+            if (ReferenceEquals(comparison, null)) throw new ArgumentNullException("comparison");
+            return BinaryFinder<T>(array, key, 0, array.Length, comparison);
+        }
+        private static int BinaryFinder<T>(T[] array, T key, int left, int right, Comparison<T> comparison)
+        {
+            if (left >= right || right == 0) return -1;
+            if (ReferenceEquals(array, null)) throw new ArgumentNullException("array");
+            if (!(key is IComparable<T>)) throw new ArgumentException("key is invalid");
+            if (ReferenceEquals(comparison, null)) comparison = Compare;
+            int mid = left + (right - left) / 2;
+            if (comparison(array[mid], key) == 0) return mid;
+            else if (comparison(array[mid], key) > 0) 
+            return BinaryFinder(array, key, left, mid, comparison);
+            else return BinaryFinder<T>(array, key, mid + 1, right, comparison);
+        }
+        private static int Compare<T>(T firstN, T secondN)
+        {
+            if (ReferenceEquals(firstN, null)) return -1;
+            if (ReferenceEquals(firstN, secondN)) return 0;
+
+            IComparable<T> compare = firstN as IComparable<T>;
+            return compare.CompareTo(secondN);
+        }       
+    }
+}
---
+++ ASP.NET.Zanevskaya.Day7/Task4.Test/BinarySearchTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task4.Library;
+
+namespace Task4.Test
+{
+    [TestClass]
+    public class BinarySearchTest
+    {
+        [TestMethod]
+        public void BinarySearch_UsualDateInt_Test()
+        {
+            int[] numbers = { -12, 0, 23, 24, 345 };
+            int expected = 0;
+
+            Assert.AreEqual(expected, BinarySearch.Binary(numbers, -12));
+        }
+        [TestMethod]
+        public void BinarySearch_ReturnFalseInt_Test()
+        {
+            int[] numbers = { -12, 0, 23, 24, 345 };
+            int expected = -1;
+
+            Assert.AreEqual(expected, BinarySearch.Binary(numbers, -13));
+        }
+        [TestMethod]
+        public void BinarySearch_UsualDateString_Test()
+        {
+            string[] dinosaurs = {"Amargasaurus", 
+                                "Pachycephalosaurus", 
+                              "Tyrannosaurus", 
+                              "Mamenchisaurus", 
+                              "Deinonychus", 
+                              "Edmontosaurus"};
+            int expected = 0;
+
+            Assert.AreEqual(expected, BinarySearch.Binary(dinosaurs, "Amargasaurus"));
+        }
+        [TestMethod]
+        public void BinarySearch_ReturnFalseIString_Test()
+        {
+            
+            string[] dinosaurs = {"Amargasaurus", 
+                                "Pachycephalosaurus", 
+                              "Tyrannosaurus", 
+                              "Mamenchisaurus", 
+                              "Deinonychus", 
+                              "Edmontosaurus"};
+            int expected = -1;
+
+            Assert.AreEqual(expected, BinarySearch.Binary(dinosaurs, "13"));
+        }
+    }
+}
