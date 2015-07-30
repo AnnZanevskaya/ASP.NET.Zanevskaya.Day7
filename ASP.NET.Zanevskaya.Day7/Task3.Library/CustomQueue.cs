de2437f6210635a822b2d5c1720d18c7a635db6b
Ann Zanevskaya <ann.zanevskaya7@gmail.com>
2015-07-30 13:48:35 +0300

    CustomQueue

---
+++ ASP.NET.Zanevskaya.Day7/Task3.Library/CustomQueue.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Library
+{
+    public class CustomQueue<T>
+    {
+        private T[] array;
+        private int head;       
+        private int tail;       
+        private int size;       
+        static T[] emptyArray = new T[0];
+        public int Count { get { return size; } }
+
+        public CustomQueue()
+        {
+            array = emptyArray;
+        }
+        public CustomQueue(int capacity) 
+        {
+            if (capacity < 0) throw new ArgumentException("capacity");
+            array = new T[capacity];
+            head = 0;
+            tail = 0;
+            size = 0;
+        }
+        public void Enqueue(T item)
+        {
+            if (size == array.Length)
+            {
+                int newcapacity = array.Length * 3;
+                if (newcapacity < array.Length + 4)
+                {
+                    newcapacity = array.Length + 4;
+                }
+                T[] newQueue = new T[newcapacity];
+                SetCapacity(newcapacity);
+            }
+
+            array[tail] = item;
+            tail = (tail + 1) % array.Length;
+            size++;
+        }
+        public T Dequeue()
+        {
+            if (size == 0) throw new ArgumentException("array is empty");
+                
+            T removed = array[head];
+            array[head] = default(T);
+            head = (head + 1) % array.Length;
+            size--;
+            return removed;
+        }
+        public T Peek()
+        {
+            if (size == 0) throw new ArgumentException("array is empty");
+            return array[head];
+        }
+        public CustomQueueEnumerator GetEnumerator()
+        {
+            return new CustomQueueEnumerator(this);
+        }
+
+        public struct CustomQueueEnumerator
+        {
+            private CustomQueue<T> q;
+            private int index;
+            private T currentElement;
+
+            public CustomQueueEnumerator(CustomQueue<T> q)
+            {
+                this.q = q;
+                index = -1;
+                currentElement = default(T);
+            }
+            public T Current {
+                get {
+                    if (index < 0) throw new ArgumentException();
+                    return currentElement;
+                }
+            }
+            
+             public void Dispose()
+            {
+                index = -2;
+                currentElement = default(T);
+            }
+            public bool MoveNext() {  
+                if (index == -2) return false;
+                index++;
+                if (index == q.size) {
+                    index = -2;
+                    currentElement = default(T);
+                    return false;
+                }
+
+                currentElement = q.GetElement(index);
+                return true;
+            }
+            public void Reset()
+            {
+                index = -1;
+                currentElement = default(T);
+            }
+        }
+        private T GetElement(int i)
+        {
+            return array[(head + i) % array.Length];
+        }
+        private void SetCapacity(int capacity)
+        {
+            T[] newarray = new T[capacity];
+            if (size > 0)
+            {
+                if (head < tail)
+                {
+                    Array.Copy(array, head, newarray, 0, size);
+                }
+                else
+                {
+                    Array.Copy(array, head, newarray, 0, array.Length - head);
+                    Array.Copy(array, 0, newarray, array.Length - head, tail);
+                }                  
+            }
+            array = newarray;
+            head = 0;
+            tail = (size == capacity) ? 0 : size;
+        }
+
+    }
+}
---
+++ ASP.NET.Zanevskaya.Day7/Task3.Test/CustomQueueTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task3.Library;
+using System.Collections.Generic;
+
+namespace Task3.Test
+{
+    [TestClass]
+    public class CustomQueueTest
+    {
+        [TestMethod]
+        public void CustomQueue_UsualDate_EnqueueTest()
+        {
+            CustomQueue<int> queue = new CustomQueue<int>(1);
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+
+            Assert.AreEqual(2, queue.Count);
+        }
+        [TestMethod]
+        public void CustomQueue_UsualDaten_DequeueTest()
+        {
+            CustomQueue<int> queue = new CustomQueue<int>(1);
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Dequeue();
+            queue.Dequeue();
+
+            Assert.AreEqual(0, queue.Count);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CustomQueue_Exeption_DequeueTest()
+        {
+            CustomQueue<int> queue = new CustomQueue<int>(1);
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+        }
+        [TestMethod]
+        public void CustomQueue_Foreach_Test()
+        {
+            CustomQueue<int> queue = new CustomQueue<int>(1);
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            int count = 0;
+            foreach (var item in queue)
+            {
+                count++;
+            }
+            Assert.AreEqual(2, count);
+        }
+        [TestMethod]
+        public void CustomQueue_Peek_Test()
+        {
+            CustomQueue<int> queue = new CustomQueue<int>(1);
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+
+            var item = queue.Peek();
+
+            Assert.AreEqual(1, item);
+        }
+    }
+}
